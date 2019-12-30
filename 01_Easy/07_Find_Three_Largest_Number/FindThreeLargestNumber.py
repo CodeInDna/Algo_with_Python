10a2f8b35a5135ab30e011d5e1498336d0149e45
CodeInDna <ykarelia323@gmail.com>
2019-12-30 17:37:38 +0530

    Find Three Largest Numbers

--- 01_Easy/07_Find_Three_Largest_Number/FindThreeLargestNumber.py
+++ 01_Easy/07_Find_Three_Largest_Number/FindThreeLargestNumber.py
@@ -1,4 +1,4 @@
-# ---------------------------------- PROBLEM 06 (EASY) --------------------------------------#
+# ---------------------------------- PROBLEM 07 (EASY) --------------------------------------#
 # Find Three Largest Numbers
 # Write a function that takes in an array of integers and returns a sorted array of the three 
 # largest integers in the input array. Note that the function should return duplicate integers 
