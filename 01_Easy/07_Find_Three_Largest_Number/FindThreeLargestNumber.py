ef64ad425892463ec79c2d129312197526827007
CodeInDna <ykarelia323@gmail.com>
2019-12-30 16:13:51 +0530

    Find three Largest Numbers from the list

--- 01_Easy/07_Find_Three_Largest_Number/FindThreeLargestNumber.py
+++ 01_Easy/07_Find_Three_Largest_Number/FindThreeLargestNumber.py
@@ -14,10 +14,36 @@
 def findThreeLargestNumbers(lst):
 	sorted_lst = sorted(lst)
 	return sorted_lst[-3:]
-print(findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]))
 # ----------------METHOD 01---------------------#
 
 # ----------------METHOD 02---------------------#
-# COMPLEXITY = TIME: O(), SPACE: O()
+# COMPLEXITY = TIME: O(n), SPACE: O(1)
+def findThreeLargestNumbers(lst):
+# Iterate over the given list
+	threeLargestNums = [None, None, None]
+	for num in lst:
+		updateLargest(threeLargestNums, num)
+	return threeLargestNums
 
+def updateLargest(threeLargestNums, num):
+# Check if the number is greater than the one of the three numbers in threeLargestNums list
+	if threeLargestNums[2] is None or threeLargestNums[2] < num:
+		shiftAndUpdate(threeLargestNums, num, 2)
+	elif threeLargestNums[1] is None or threeLargestNums[1] < num:
+		shiftAndUpdate(threeLargestNums, num, 1)
+	elif threeLargestNums[0] is None or threeLargestNums[0] < num:
+		shiftAndUpdate(threeLargestNums, num, 0)
+	return threeLargestNums
+
+def shiftAndUpdate(threeLargestNums, num, idx):
+# Storing and Updating the number
+	for i in range(idx + 1):
+		if i == idx:
+			threeLargestNums[i] = num
+		else:
+			threeLargestNums[i] = threeLargestNums[i+1]
+	return threeLargestNums
 # ----------------METHOD 02---------------------#
+
+print(findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]))
+print(findThreeLargestNumbers([-1, -2, -3, -7, -17, -27, -18, -541, -8, -7, 7]))
