9d9793c9bf0710f5a14561774856199a84221b85
CodeInDna <ykarelia323@gmail.com>
2019-12-31 18:04:15 +0530

    Bubble Sort with Test Cases

---
+++ 01_Easy/09_Bubble_Sort/Bubble_Sort.py
@@ -0,0 +1,42 @@
+# ---------------------------------- PROBLEM 09 (EASY) --------------------------------------#
+# Palindrome Check
+# Write a function that takes in array and returns a sorted array.
+# USe buuble Sort to sort items.
+
+# Sample input: [3, 1, 2]
+# Sample output: [1, 2, 3]
+
+# ----------------METHOD 01---------------------#
+# BEST: COMPLEXITY = TIME: O(n), SPACE: O(1) 
+# WORST/AVERAGE: COMPLEXITY = TIME: O(n^2), SPACE: O(1) 
+def bubbleSort(lst):
+	for i in range(len(lst)-1, 0, -1):
+		noSwaps = False
+		for j in range(i):
+			if lst[j] > lst[j+1]:
+				lst = swap(j, j+1, lst)
+				noSwaps = True
+		if not noSwaps: return lst 
+	return lst
+
+def swap(idx1, idx2, lst):
+	lst[idx1], lst[idx2] = lst[idx2], lst[idx1]
+	return lst
+# ----------------METHOD 01---------------------#
+
+
+# ----------------METHOD 02 (USING WHILE LOOP)---------------------#
+# BEST: COMPLEXITY = TIME: O(n), SPACE: O(1) 
+# WORST/AVERAGE: COMPLEXITY = TIME: O(n^2), SPACE: O(1) 
+def bubbleSort(lst):
+	noSwaps = False
+	counter = 0 
+	while not noSwaps:
+		noSwaps = True
+		for i in range(len(lst) - 1 - counter):
+			if lst[i] > lst[i+1]:
+				swap(i, i+1, lst)
+				noSwaps = False
+		counter += 1
+	return lst	
+# ----------------METHOD 02---------------------#
