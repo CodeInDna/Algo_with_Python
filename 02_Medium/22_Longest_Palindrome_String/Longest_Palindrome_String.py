ed88a07eb0d502824c82e3fcfa3f48f39a2c1902
CodeInDna <ykarelia323@gmail.com>
2020-03-31 17:10:01 +0530

    Longest Palindrome String with Test Cases

--- 02_Medium/22_Longest_Palindrome_String/Longest_Palindrome_String.py
+++ 02_Medium/22_Longest_Palindrome_String/Longest_Palindrome_String.py
@@ -39,3 +39,24 @@ def isPalindrome(string):
 		rightIdx -= 1
 	return True
 # ----------------METHOD 02---------------------#
+
+# ----------------METHOD 03---------------------#
+# TOTAL COMPLEXITY = TIME: O(n^2), SPACE: O(1)
+def longestPalindromicSubstring(string):
+	currentLongest = [0, 1]
+	for i in range(len(string)): 
+		odd = getLongestPalindromeFrom(string, i-1, i+1)
+		even = getLongestPalindromeFrom(string, i-1, i)
+		longest = max(odd, even, key = lambda x:x[1] - x[0])
+		currentLongest = max(longest, currentLongest, key = lambda x:x[1] - x[0])
+		print(odd, even, longest, currentLongest)
+	return string[currentLongest[0]:currentLongest[1]]
+		
+def getLongestPalindromeFrom(string, leftIdx, rightIdx):
+	while leftIdx >= 0 and rightIdx < len(string):
+		if string[leftIdx] != string[rightIdx]:
+			break
+		leftIdx -= 1
+		rightIdx += 1
+	return [leftIdx + 1, rightIdx]
+# ----------------METHOD 03---------------------#
