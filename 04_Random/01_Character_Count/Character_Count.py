92bf8fc1d808b18926681e75d6d4a63adc09fff5
CodeInDna <ykarelia323@gmail.com>
2020-06-15 23:13:27 +0530

    Character frequency Counter: Method 2 O(n)

--- 04_Random/01_Character_Count/Character_Count.py
+++ 04_Random/01_Character_Count/Character_Count.py
@@ -7,20 +7,36 @@
 
 # ----------------METHOD 01---------------------#
 # COMPLEXITY = TIME: O(n), SPACE: O(n)
+# def char_count(string):
+# 	# if string is empty return False
+# 	if not string: return False
+
+# 	# define an object to return in the end
+# 	charCount = {}
+
+# 	# iterate over the lowercase characters 
+# 	for char in string.lower():
+# 	# check if char is alphanumeric
+# 		if char.isalnum(): 
+# 		# if char not in object, initialize it with 1, else add 1
+# 			charCount[char] = charCount[char] + 1 if char in charCount else 1
+
+# 	# return object with char count
+# 	return charCount
+# ----------------METHOD 01---------------------#
+
+
+# ----------------METHOD 02---------------------#
+# COMPLEXITY = TIME: O(n), SPACE: O(n)
 def char_count(string):
 	# if string is empty return False
 	if not string: return False
 
 	# define an object to return in the end
-	charCount = {}
+	str_lower = string.lower()
 
-	# iterate over the lowercase characters 
-	for char in string.lower():
-	# check if char is alphanumeric
-		if char.isalnum(): 
-		# if char not in object, initialize it with 1, else add 1
-			charCount[char] = charCount[char] + 1 if char in charCount else 1
-
+	charCount = {char: str_lower.count(char) for char in str_lower if char.isalnum()}
+	
 	# return object with char count
 	return charCount
-# ----------------METHOD 01---------------------#
+# ----------------METHOD 02---------------------#
