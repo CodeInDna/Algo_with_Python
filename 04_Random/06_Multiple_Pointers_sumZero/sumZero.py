a87632e13b77cb96c37772089932c7d98fc69b15
CodeInDna <ykarelia323@gmail.com>
2020-06-23 16:21:06 +0530

    Multiple Pointers: sumZero

--- 04_Random/06_Multiple_Pointers_sumZero/sumZero.py
+++ 04_Random/06_Multiple_Pointers_sumZero/sumZero.py
@@ -1,4 +1,4 @@
-# ---------------------- PROBLEM 04 (RANDOM) ----------------------------------#
+# ---------------------- PROBLEM 06 (RANDOM) ----------------------------------#
 # Write a function called sumZero, which accepts a sorted array of integers.
 # The function should find the first pair where the sum is 0. Return an array that
 # includes both values that sum to zero or undefined if pair doesn't exist.
