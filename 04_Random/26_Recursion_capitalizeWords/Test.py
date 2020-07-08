cf78b08a1add531ddf368e7abbb69b2917b446e8
CodeInDna <ykarelia323@gmail.com>
2020-07-08 21:09:15 +0530

    Recursion: Capitalize Word Test Cases

--- 04_Random/26_Recursion_capitalizeWords/Test.py
+++ 04_Random/26_Recursion_capitalizeWords/Test.py
@@ -1,17 +1,6 @@
 import unittest
 from capitalizeWords import capitalizeWords
 
-dict2 = {'num': 1,
-		'test': [],
-		'data': {
-			'val': 4,
-			'info':{
-				'isRight': True
-				'random': 66
-				}
-			}
-		}
-		
 class TestProgram(unittest.TestCase):
 	def test_case_1(self):
 		self.assertEqual(capitalizeWords(['taco', 'banana', 'car']), ['TACO', 'BANANA', 'CAR'])
