11341a5f8b4942474ccf5c3ebea6003a7bd97ecb
CodeInDna <ykarelia323@gmail.com>
2020-07-19 17:19:14 +0530

    remove and reverse sll

--- 04_Random/38_DS_singlyLinkedList/singlyLinkedList.py
+++ 04_Random/38_DS_singlyLinkedList/singlyLinkedList.py
@@ -68,8 +68,89 @@ class SLL:
 		return currNode.val
 	# ----------------METHOD 04---------------------#
 
-sll_1 = SLL().push(1).push(2).push(3)
-sll_1.shift()
-sll_1.shift()
+	# ----------------METHOD 05---------------------#
+	# COMPLEXITY = TIME: O(1), SPACE: O(1)
+	def unshift(self, val):
+		newNode = Node(val)
+		if not self.head:
+			self.head = newNode
+			self.tail = newNode
+		else:
+			newNode.next = self.head
+			self.head = newNode
+		self.length += 1
+		return self
+	# ----------------METHOD 05---------------------#
 
-sll_1.traverse()
+	# ----------------METHOD 06---------------------#
+	# COMPLEXITY = TIME: O(n), SPACE: O(1)
+	def get(self, idx):
+		if idx < 0 or idx >= self.length: return None
+
+		currNode = self.head
+		count = 0 
+		while count != idx:
+			count += 1
+			currNode = currNode.next
+		return currNode
+	# ----------------METHOD 06---------------------#
+
+	# ----------------METHOD 07---------------------#
+	# COMPLEXITY = TIME: O(n), SPACE: O(1)
+	def set(self, idx, val):
+		currNode = self.get(idx)
+		if currNode:
+			currNode.val = val
+		return True
+	# ----------------METHOD 07---------------------#
+
+	# ----------------METHOD 08---------------------#
+	# COMPLEXITY = TIME: O(n), SPACE: O(1)
+	def insert(self, idx, val):
+		if idx < 0 or idx > self.length:
+			return False
+		if idx == 0:
+			return unshift(val)
+		if idx == self.length:
+			return push(val)
+
+		newNode = Node(val)
+		prevNode = self.get(idx - 1)
+		newNode.next = prevNode.next
+		prevNode.next = newNode
+		self.length += 1
+
+		return True
+	# ----------------METHOD 08---------------------#
+
+	# ----------------METHOD 09---------------------#
+	# COMPLEXITY = BEST TIME: O(1), WORST TIME: O(n) SPACE: O(1)
+	def remove(self, idx):
+		if idx < 0 or idx >= self.length:
+			return None
+		if idx == 0:
+			return self.shift()
+		if idx == self.length-1:
+			return self.pop()
+
+		prevNode = self.get(idx - 1)
+		removed = prevNode.next
+		prevNode.next = removed.next
+		self.length -= 1
+
+		return removed
+	# ----------------METHOD 09---------------------#
+
+	# ----------------METHOD 10---------------------#
+	# COMPLEXITY = TIME: O(n), SPACE: O(1)
+	def reverse(self):
+		currNode = self.head
+		prevNode = None
+		self.head, self.tail = self.tail, self.head
+		while currNode is not None:
+			nextNode = currNode.next
+			currNode.next = prevNode
+			prevNode = currNode
+			currNode = nextNode
+		return self
+	# ----------------METHOD 10---------------------#
