dbd5f5de976292a74f59f62b7dcd46047950216c
CodeInDna <ykarelia323@gmail.com>
2020-08-17 15:22:28 +0530

    Djikstra Algo

--- 04_Random/49_Djikstras_shortest_path/dijkstrasAlgo.py
+++ 04_Random/49_Djikstras_shortest_path/dijkstrasAlgo.py
@@ -21,6 +21,43 @@ class WeightedGraph:
 		return self.adjacencyList
 # ----------------METHOD 02---------------------#
 
+# ----------------METHOD 03---------------------#
+	# COMPLEXITY = TIME: O(V log V+E), SPACE: O(V^2)
+	def Dijkstra(self, start, finish):
+		nodes = PriorityQueue()
+		distances = {}
+		previous = {}
+		smallest = None
+		path = []
+
+		# build up initiale state
+		for vertex in self.adjacencyList:
+			if vertex == start:
+				distances[vertex] = 0
+				nodes.enqueue(vertex, 0)
+			else: 
+				distances[vertex] = float("inf")
+				nodes.enqueue(vertex, float("inf"))
+			previous[vertex] = None
+
+		while len(nodes.values) != 0:
+			smallest = nodes.dequeue()["val"]
+			if smallest == finish:
+				while smallest != None:
+					path.append(smallest)
+					smallest = previous[smallest]
+				break
+
+			for neigh in self.adjacencyList[smallest]:
+				candidate = distances[smallest] + neigh["weight"]
+				if candidate < distances[neigh["node"]]:
+					distances[neigh["node"]] = candidate
+					previous[neigh["node"]] = smallest
+					nodes.enqueue(neigh["node"], candidate)
+		return path[::-1]
+# ----------------METHOD 03---------------------#
+
+
 class PriorityQueue:
 	def __init__(self):
 		self.values = []
@@ -31,18 +68,29 @@ class PriorityQueue:
 		return self.values
 
 	def dequeue(self):
-		return self.pop(0)
+		return self.values.pop(0)
 
 	def sort(self):
-		sorted(self.values, key = lambda x: x['priority']) 
+		self.values = sorted(self.values, key = lambda i: i['priority']) 
+
+
 
 weightedGraph = WeightedGraph()
 weightedGraph.addVertex("A")
 weightedGraph.addVertex("B")
 weightedGraph.addVertex("C")
-print(weightedGraph.addEdge("A", "B", 250))
-print(weightedGraph.addEdge("C", "B", 30))
+weightedGraph.addVertex("D")
+weightedGraph.addVertex("E")
+weightedGraph.addVertex("F")
+weightedGraph.addEdge("A", "B", 4)
+weightedGraph.addEdge("A", "C", 2)
+weightedGraph.addEdge("C", "D", 2)
+weightedGraph.addEdge("C", "F", 4)
+weightedGraph.addEdge("D", "E", 3)
+weightedGraph.addEdge("D", "F", 1)
+weightedGraph.addEdge("E", "F", 1)
+weightedGraph.addEdge("E", "B", 3)
 
-priority = PriorityQueue()
-priority.enqueue("A", 2)
-print(priority.enqueue("B", 1))
+print(weightedGraph.Dijkstra("A", "E"))
+
+
